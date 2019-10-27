c7943395de5f99275b42227a31134685c19a049e
MOSSOLBMULP <52730414+MOSSOLBMULP@users.noreply.github.com>
2019-10-27 21:07:22 +0800

    根据编程规范添加注释
    根据编程规范检查格式，添加注释，增加可读性。

--- include/anglesolver.h
+++ include/anglesolver.h
@@ -3,9 +3,10 @@
 
  Author: Ma Yuemeng & Song Haodi
 
- Update: Wang Xiaoyan on 2019.7.1.
-　　　　 Zeng Jing on 2019.10.21.
- Detail:
+ Update: Wang Xiaoyan on 2019.7.1
+         Zeng Jing on 2019.10.27.
+
+ Detail:弹道计算
  *****************************************************************************/
 
 #ifndef HERORM2020_ANGLESOLVER_H
@@ -17,6 +18,7 @@
 #ifdef RUNNING_TIME
 #include "timer.h"
 #endif
+//#define DEBUG
 
 
 class AngleSolver {
@@ -28,10 +30,14 @@ public:
     ~AngleSolver();
     void init();
 
+    //弹道计算，根据坐标值计算出云台需调整的角度值，输入坐标值和初速度、云台绝对pitch角，返回pitch角和yaw角
     void newrun(double x, double y, double z, double v,double& yaw, double& pitch, double ptz_pitch);
-    void newdichotomy(double x, double y, double z, double v,double& yaw, double& pitch);
+    //将坐标转化回标准坐标系，然后二分法计算角度
+    void newdichotomy(double x, double y, double z, double v,double& yaw, double& pitch);//无坐标转化的二分法
     void speedtriangle(double x, double y, double z, double v,double& yaw, double& pitch);
-    void run(double x, double y, double z, double v, double &yaw, double &pitch);
+    //无坐标转化的有速度矢量三角推导出的公式实现
+    void run(double x, double y, double z, double v, double &yaw, double &pitch);//上届算法
+
     double get_yaw_offset();
     double get_pitch_offset();
     void set_yaw_offset(double yaw_offset);
@@ -40,8 +46,6 @@ public:
 private:
     bool parabolaSolve(double x, double y, double v, double &theta);
     double parabolaDeltaY(double x, double y, double v, double theta);
-
 };
-
 
 #endif  // HERORM2020_ANGLESOLVER_H
