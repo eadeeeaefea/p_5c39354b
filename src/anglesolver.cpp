beabf13ef7cf0effcd73fb7d3159f2629b1bc6c1
MOSSOLBMULP <52730414+MOSSOLBMULP@users.noreply.github.com>
2019-10-27 21:01:11 +0800

    根据编程规范修改格式和变量名称
     根据编程规范修改格式和变量名称，添加注释，增加可读性。

--- src/anglesolver.cpp
+++ src/anglesolver.cpp
@@ -2,9 +2,9 @@
  Copyright© HITwh HERO-Robomaster2020 Group
 
  Author: Wang Xiaoyan on 2019.9.20
- 　　　　Zeng Jing on 2019.10.23.
+         Zeng Jing on 2019.10.27.
 
- Detail:
+ Detail:弹道计算
  *****************************************************************************/
 
 #include "anglesolver.h"
@@ -27,72 +27,81 @@ void AngleSolver::init() {
 }
 
 void AngleSolver::newrun(double x, double y, double z, double v,
-                               double& yaw, double& pitch, double ptz_pitch) {
+                         double& yaw, double& pitch, double ptz_pitch){
     Timer timer;
     timer.start();
- 
-/*
-  positive direction of axis Y in PNP
-  and this part are converse
-*/
+
+    //之前由图像坐标系转换得到的相机坐标系的y轴竖直向下，现在转换回来
     y = -y;
- 
- double y1,z1,ptz;
-    y1=y;
-    z1=z;
-    ptz=(ptz_pitch/180)*PI;
-    y=y1*cos(ptz)+z1*sin(ptz);
-    z=z1*cos(ptz)-y1*sin(ptz);//将坐标旋转回标准坐标系(ｙ轴竖直向下）
 
-    yaw = (atan(x / z) / PI) * 180 + yaw_offset_;//计算ｙａｗ轴
+    //将云台坐标旋转回标准坐标系(ｙ轴竖直向下）
+    double last_y,last_z,ptz_degree;
+    last_y=y;//存储原来的y和z值
+    last_z=z;
+    ptz_degree=(ptz_pitch/180)*PI;//将云台绝对pitch轴转换成角度值
+    y=last_y*cos(ptz_degree)+last_z*sin(ptz_degree);
+    z=last_z*cos(ptz_degree)-last_y*sin(ptz_degree);
+    //将原来的坐标转换成标准坐标系,坐标系向下旋转一定角度等价于点向上旋转相同角度
 
-    /*caculate pitch_max and pitch_min*/
-    double pitch_min, pitch_max, mid;
+    yaw = (atan(x / z) / PI) * 180 + yaw_offset_;//计算yaw值
 
+    //算出二分法的角度范围
+    double pitch_min, pitch_max;
     pitch_min = atan2(y, sqrt(x * x + z * z))-0.05;
     pitch_max = pitch_min+0.6;
-    //cout << "pitch_min=" << pitch_min << endl;
-    //cout << "pitch_max=" << pitch_max << endl;//ｄｅｂｕｇ备用
+    //计算上限和下限角度对应落点与目标纵坐标差值（正常应异号）
+    double t_min, t_max, delta_y_min, delta_y_max, differ_min, differ_max;
+    //最小角度对应差值计算
+    t_min = sqrt(x * x + z * z) / (v * cos(pitch_min));
+    delta_y_min = v * sin(pitch_min) * t_min - 0.5 * g * t_min * t_min;
+    differ_min = delta_y_min - y;
+    //最大角度对应差值计算
+    t_max = sqrt(x * x + z * z) / (v * cos(pitch_max));
+    delta_y_max = v * sin(pitch_max) * t_max - 0.5 * g * t_max*t_max;
+    differ_max = delta_y_max - y;
+#ifdef DEBUG
+    cout<<"differ_min ="<<differ_min<<endl;
+    cout<<"differ_max ="<<differ_max<<endl;
+#endif
 
-    double t1, t2, delta_y1, delta_y2, differ1, differ2;//计算上限和下限角度对应落点与目标差值（正常应一正一负）
-    t1 = sqrt(x * x + z * z) / (v * cos(pitch_min));
-    delta_y1 = v * sin(pitch_min) * t1 - 0.5 * g * t1 * t1;
-    differ1 = delta_y1 - y;
+    //提前排除不需二分的情况:
+    if (fabs(differ_min) <= 0.001) {
+        //最小角度符合pitch角要求：落点与目标纵坐标差值小于一定范围
+        pitch = pitch_min;
+    }else if (fabs(differ_max) <= 0.001) {//最大角度符合pitch角要求
+        pitch = pitch_max;
+    }else if (differ_min * differ_max > 0){ //两角度对应目标差值同号，出错
+        return;
+    }
 
-    t2 = sqrt(x * x + z * z) / (v * cos(pitch_max));
-    delta_y2 = v * sin(pitch_max) * t2 - 0.5 * g * t2 * t2;
-    differ2 = delta_y2 - y;
-
-    //cout << "differ1=" << differ1 << endl;
-    //cout << "differ2=" << differ2 << endl;//debug备用
-
-    if (fabs(differ1) <= 0.001) { pitch = pitch_min; }
-    else if (fabs(differ2) <= 0.001) { pitch = pitch_max; }
-    else if (differ1 * differ2 > 0) { return; }//提前排除不需二分的情况
-
-/*dichotomy,loop for several times until differ is small enough*/
-    if (differ1 * differ2 < 0) {
+    double mid;//存储二分法角度中间值的变量
+    //进行二分计算，角度对应落点与目标纵坐标差值小于一定范围
+    // 或当循环计算次数达到上限时输出pitch值
+    if (differ_min * differ_max < 0) {
         int i = 0;
-        double delta_y = 0, t = 0, differ = 0;
-        for (i = 0; i < 20; i++) {
+        double delta_y_mid = 0, t_mid= 0, differ_mid = 0;
+        for (i = 0; i < 20; i++){
+            //二分，计算mid角对应的落点与目标纵坐标差值
             mid = (pitch_max + pitch_min) / 2;
-            t = sqrt(x * x + z * z) / (v * cos(mid));
-            delta_y = v * sin(mid) * t - 0.5 * g * t * t;
-            differ = delta_y - y;
-            if (fabs(differ) <= 0.001 && (i == 19)) {
+            t_mid = sqrt(x * x + z * z) / (v * cos(mid));
+            delta_y_mid = v * sin(mid) * t_mid - 0.5 * g * t_mid * t_mid;
+            differ_mid = delta_y_mid - y;
+            //判断mid角度是否符合pitch角要求：落点与目标纵坐标差值小于一定范围
+            //或循环次数达到上限，若符合输出mid值作为pitch值
+            if (fabs(differ_mid) <= 0.001 && (i == 19)) {
                 pitch = mid;
                 break;
-            } else if (differ * differ1 > 0) {
+            } else if (differ_mid * differ_min > 0) {
+                //若不符合要求，缩小角度范围继续二分
                 pitch_min = mid;
             } else {
                 pitch_max = mid;
             }
         }
     }
-
-
-    pitch=(pitch/PI)*180+pitch_offset_-ptz_pitch;//计算得ｐｉｔｃｈ轴角度需减去云台绝对角度
-
+    //计算得pitch轴角度换算成角度，减去云台绝对角度才得到相对现有云台坐标系需调整角度，再加上偏移
+    pitch=(pitch/PI)*180+pitch_offset_-ptz_pitch;
+    //输出运算结果和运行时间
     cout << "Anglesolver newrun:  yaw=" << yaw << "  pitch=" << pitch << endl;
     cout << "Anglesolver newrun: " << timer.getTime() << "ms" << endl;
     timer.stop();
@@ -117,108 +126,104 @@ void AngleSolver::set_pitch_offset(double pitch_offset) {
 
 // 未加坐标旋转变换，稍微慢一些的两个算法，速度矢量三角计算快于二分法。
 //二分法算法原理同上
-  void AngleSolver::newdichotomy(double x, double y, double z, double v,
-                      double& yaw, double& pitch) {
-      Timer timer;
-      timer.start();
+void AngleSolver::newdichotomy(double x, double y, double z, double v,
+                               double& yaw, double& pitch) {
+    Timer timer;
+    timer.start();
+    //之前由图像坐标系转换得到的相机坐标系的y轴竖直向下，现在转换回来
+    y = -y;
 
-      double temp_pitch_offset = pitch_offset_;
+    //计算yaw值
+    yaw = (atan(x / z) / PI) * 180 + yaw_offset_;
 
-/*
-  positive direction of axis Y in PNP
-  and this part are converse
-*/
-      y = -y;
-      yaw = (atan(x / z) / PI) * 180 + yaw_offset_;
+    //算出二分法的角度范围
+    double pitch_min, pitch_max;
+    pitch_min = atan2(y, sqrt(x * x + z * z))-0.05;
+    pitch_max = pitch_min+0.6;
+    //计算上限和下限角度对应落点与目标纵坐标差值（正常应异号）
+    double t_min, t_max, delta_y_min, delta_y_max, differ_min, differ_max;
+    //最小角度对应差值计算
+    t_min = sqrt(x * x + z * z) / (v * cos(pitch_min));
+    delta_y_min = v * sin(pitch_min) * t_min - 0.5 * g * t_min * t_min;
+    differ_min = delta_y_min - y;
+    //最大角度对应差值计算
+    t_max = sqrt(x * x + z * z) / (v * cos(pitch_max));
+    delta_y_max = v * sin(pitch_max) * t_max - 0.5 * g * t_max*t_max;
+    differ_max = delta_y_max - y;
+#ifdef DEBUG
+    cout<<"differ_min ="<<differ_min<<endl;
+    cout<<"differ_max ="<<differ_max<<endl;
+#endif
 
-      /*caculate pitch_max and pitch_min*/
-      double pitch_min, pitch_max, mid;
+    //提前排除不需二分的情况:
+    if (fabs(differ_min) <= 0.001) {
+        //最小角度符合pitch角要求：落点与目标纵坐标差值小于一定范围
+        pitch = pitch_min;
+    }else if (fabs(differ_max) <= 0.001) {//最大角度符合pitch角要求
+        pitch = pitch_max;
+    }else if (differ_min * differ_max > 0){ //两角度对应目标差值同号，出错
+        return;
+    }
 
-      pitch_min = atan2(y, sqrt(x * x + z * z))-0.05;
-      pitch_max = pitch_min+0.6;
-      //cout << "pitch_min=" << pitch_min << endl;
-      //cout << "pitch_max=" << pitch_max << endl;
-
-      double t1, t2, delta_y1, delta_y2, differ1, differ2;
-      t1 = sqrt(x * x + z * z) / (v * cos(pitch_min));
-      delta_y1 = v * sin(pitch_min) * t1 - 0.5 * g * t1 * t1;
-      differ1 = delta_y1 - y;
-
-      t2 = sqrt(x * x + z * z) / (v * cos(pitch_max));
-      delta_y2 = v * sin(pitch_max) * t2 - 0.5 * g * t2 * t2;
-      differ2 = delta_y2 - y;
-
-      //cout << "differ1=" << differ1 << endl;
-      //cout << "differ2=" << differ2 << endl;
-
-      if (fabs(differ1) <= 0.001) { pitch = pitch_min; }
-      else if (fabs(differ2) <= 0.001) { pitch = pitch_max; }
-      else if (differ1 * differ2 > 0) { return; }
-
-/*dichotomy,loop for several times until differ is small enough*/
-      if (differ1 * differ2 < 0) {
-          int i = 0;
-          double delta_y = 0, t = 0, differ = 0;
-          for (i = 0; i < 20; i++) {
-              mid = (pitch_max + pitch_min) / 2;
-              t = sqrt(x * x + z * z) / (v * cos(mid));
-              delta_y = v * sin(mid) * t - 0.5 * g * t * t;
-              differ = delta_y - y;
-              if (fabs(differ) <= 0.001 && (i == 19)) {
-                  pitch = mid;
-                  break;
-              } else if (differ * differ1 > 0) {
-                  pitch_min = mid;
-              } else {
-                  pitch_max = mid;
-              }
-          }
-          }
-
-
-          pitch = (pitch / PI) * 180 + temp_pitch_offset;
-          //pitch=(pitch/PI)*180+temp_pitch_offset-ptz_pitch;
-
-          cout << "NewDichotomy:  yaw=" << yaw << "  pitch=" << pitch << endl;
-          cout << "NewDichotomy time: " << timer.getTime() << "ms" << endl;
-          timer.stop();
-      }
-
-
-    void AngleSolver::speedtriangle(double x, double y, double z, double v,
-                                   double& yaw, double& pitch) {
-        Timer timer;
-        timer.start();
-
-        double temp_pitch_offset = pitch_offset_;
-
-/*
-        positive direction of axis Y in PNP
-                                        and this part don't need to converse
-        */
-
-
-
-        yaw = (atan(x / z) / PI )* 180 + yaw_offset_;
-
-        //由速度矢量三角形推导出的公式实现
-        double speed_angle,sin_speed_angle,pitch_tan_y,pitch_tan_x,mid_value;
-        sin_speed_angle=g*sqrt(x*x+z*z)/(v*sqrt(v*v+2*g*y));
-        speed_angle=asin(sin_speed_angle);
-        mid_value=v-cos(speed_angle)*sqrt(v*v+2*g*y);
-        pitch_tan_y=v*fabs(mid_value);
-        pitch_tan_x=g*sqrt(x*x+z*z);
-        pitch=atan2(pitch_tan_y,pitch_tan_x);
-
-        if(mid_value<0)
-        {pitch=-pitch;}
-
-
-            pitch = (pitch/PI)*180 + temp_pitch_offset;
-            cout<<"SpeedTriangle:  yaw="<<yaw<<"  pitch="<<pitch<<endl;
-            cout << "SpeedTriangle time: " << timer.getTime() << "ms" << endl;
-            timer.stop();
+    double mid;//存储二分法角度中间值的变量
+    //进行二分计算，角度对应落点与目标纵坐标差值小于一定范围
+    // 或当循环计算次数达到上限时输出pitch值
+    if (differ_min * differ_max < 0) {
+        int i = 0;
+        double delta_y_mid = 0, t_mid= 0, differ_mid = 0;
+        for (i = 0; i < 20; i++){
+            //二分，计算mid角对应的落点与目标纵坐标差值
+            mid = (pitch_max + pitch_min) / 2;
+            t_mid = sqrt(x * x + z * z) / (v * cos(mid));
+            delta_y_mid = v * sin(mid) * t_mid - 0.5 * g * t_mid * t_mid;
+            differ_mid = delta_y_mid - y;
+            //判断mid角度是否符合pitch角要求：落点与目标纵坐标差值小于一定范围
+            //或循环次数达到上限，若符合输出mid值作为pitch值
+            if (fabs(differ_mid) <= 0.001 && (i == 19)) {
+                pitch = mid;
+                break;
+            } else if (differ_mid * differ_min > 0) {
+                //若不符合要求，缩小角度范围继续二分
+                pitch_min = mid;
+            } else {
+                pitch_max = mid;
+            }
         }
+    }
+    pitch = (pitch / PI) * 180 + pitch_offset_;//换算成角度
+
+    cout << "NewDichotomy:  yaw=" << yaw << "  pitch=" << pitch << endl;
+    //输出结果和运行时间
+    cout << "NewDichotomy time: " << timer.getTime() << "ms" << endl;
+    timer.stop();
+}
+
+
+void AngleSolver::speedtriangle(double x, double y, double z, double v,
+                                double& yaw, double& pitch) {
+    Timer timer;
+    timer.start();
+    //由于算法需要，此处不需要将y轴反向
+    //计算yaw值
+    yaw = (atan(x / z) / PI )* 180 + yaw_offset_;
+
+    //由速度矢量三角形推导出的公式实现
+    double speed_angle,sin_speed_angle,pitch_tan_y,pitch_tan_x,mid_value;
+    sin_speed_angle=g*sqrt(x*x+z*z)/(v*sqrt(v*v+2*g*y));
+    speed_angle=asin(sin_speed_angle);
+    mid_value=v-cos(speed_angle)*sqrt(v*v+2*g*y);
+    pitch_tan_y=v*fabs(mid_value);
+    pitch_tan_x=g*sqrt(x*x+z*z);
+    pitch=atan2(pitch_tan_y,pitch_tan_x);
+    //由推导公式，根据中间值判断pitch角正负
+    if(mid_value<0){pitch=-pitch;}
+
+    pitch = (pitch/PI)*180 +pitch_offset_;//换算成角度
+
+    cout<<"SpeedTriangle:  yaw="<<yaw<<"  pitch="<<pitch<<endl;
+    cout << "SpeedTriangle time: " << timer.getTime() << "ms" << endl;
+    timer.stop();
+}
 
 // 上届二分法
 void AngleSolver::run(double x, double y, double z, double v,
@@ -303,3 +308,4 @@ double AngleSolver::parabolaDeltaY(double x, double y, double v, double theta) {
     // double delta_y = pow(m*g/k, 0.5) * log(1/cos(a*t+b)) / a - log(1/cos(b)) / a - y;
     return delta_y;
 }
+
