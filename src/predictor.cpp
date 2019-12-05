9414cc8b34e3abef0f9b6d2deb61f3db467c956e
ecstayalive <47520725+ecstayalive@users.noreply.github.com>
2019-12-05 22:45:32 +0800

    Add files via upload

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -28,10 +28,35 @@ void Predictor::init() {
 
 void Predictor::run(double &x, double &y, double &z, double ptz_pitch, double ptz_yaw) {
     get_excercise_time(sqrt(x * x + z * z), y, 20, ptz_pitch);
-    motion_prediction(x, y, z, ptz_pitch, ptz_yaw);
+    explosive_motion_prediction(x, y, z, ptz_pitch, ptz_yaw);
+//    normal_motion_prediction(x, y, z, ptz_pitch, ptz_yaw);
 }
 
-void Predictor::motion_prediction(double &x, double &y, double &z, double ptz_pitch, double ptz_yaw) {
+void Predictor::explosive_motion_prediction(double &x, double &y, double &z, double ptz_pitch, double ptz_yaw) {
+    double angle_pitch, angle_yaw;
+    angle_pitch = ptz_pitch * PI / 180;
+    angle_yaw = ptz_yaw * PI / 180;
+    double dis;
+    double time = 0.01;
+    coordinate_transformation(x, y, z, angle_pitch, angle_yaw);
+    object_motion = Point3d(x, y, z);
+    if (object_coordinate.size() < 2) {
+        object_coordinate.emplace_back(object_motion);
+    } else {
+        object_coordinate.erase(object_coordinate.begin());
+        object_coordinate.emplace_back(object_motion);
+        dis = point_distance(object_coordinate[0], object_coordinate[1]);
+        if (dis < 0.25 && dis > 0.03 && judgement()) {
+            object_accurate_speed = (object_coordinate[1] - object_coordinate[0]) / time;
+            x = object_coordinate[1].x + object_accurate_speed.x * time_for_excercise;
+            y = object_coordinate[1].y + object_accurate_speed.y * time_for_excercise;
+            z = object_coordinate[1].z + object_accurate_speed.z * time_for_excercise;
+        }
+    }
+    anti_coordinate_transformation(x, y, z, angle_pitch, angle_yaw);
+}
+
+void Predictor::normal_motion_prediction(double &x, double &y, double &z, double ptz_pitch, double ptz_yaw) {
     double angle_pitch, angle_yaw;
     angle_pitch = ptz_pitch * PI / 180;
     angle_yaw = ptz_yaw * PI / 180;
@@ -45,21 +70,19 @@ void Predictor::motion_prediction(double &x, double &y, double &z, double ptz_pi
     } else {
         object_coordinate.erase(object_coordinate.begin());
         object_coordinate.emplace_back(object_motion);
-    }
-    dis = point_distance(object_coordinate[8], object_coordinate[9]);
-    if (dis < 0.25 && dis > 0.017 && judgement()) {
-        for (c = 0; c <= 4; c++) {
-            object_inaccurate_speed[c] = (object_coordinate[c + 5] - object_coordinate[c]) / time;
+        dis = point_distance(object_coordinate[8], object_coordinate[9]);
+        if (dis < 0.25 && dis > 0.017 && judgement()) {
+            for (c = 0; c <= 4; c++) {
+                object_inaccurate_speed[c] = (object_coordinate[c + 5] - object_coordinate[c]) / time;
+            }
+            object_accurate_speed = (object_inaccurate_speed[0] + object_inaccurate_speed[1] + object_inaccurate_speed[2] +
+                                     object_inaccurate_speed[3] + object_inaccurate_speed[4]) / 5;
+            x = object_coordinate[9].x + object_accurate_speed.x * time_for_excercise;
+            y = object_coordinate[9].y + object_accurate_speed.y * time_for_excercise;
+            z = object_coordinate[9].z + object_accurate_speed.z * time_for_excercise;
         }
-        object_accurate_speed = (object_inaccurate_speed[0] + object_inaccurate_speed[1] + object_inaccurate_speed[2] +
-                                 object_inaccurate_speed[3] + object_inaccurate_speed[4]) / 5;
-        x = object_coordinate[9].x + object_accurate_speed.x * time_for_excercise;
-        y = object_coordinate[9].y + object_accurate_speed.y * time_for_excercise;
-        z = object_coordinate[9].z + object_accurate_speed.z * time_for_excercise;
-        anti_coordinate_transformation(x, y, z, angle_pitch, angle_yaw);
-    } else {
-        anti_coordinate_transformation(x, y, z, angle_pitch, angle_yaw);
     }
+    anti_coordinate_transformation(x, y, z, angle_pitch, angle_yaw);
 }
 
 void Predictor::coordinate_transformation(double &x, double &y, double &z, double ptz_pitch, double ptz_yaw) {
@@ -96,12 +119,12 @@ double Predictor::point_distance(Point3d point1, Point3d point2) {
 bool Predictor::judgement() {
     int i;
     double count = 0;
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < object_coordinate.size(); i++) {
         if (object_coordinate[i] != Point3d(0, 0, 0)) {
             count += 1;
         }
     }
-    if (count == 10) {
+    if (count == object_coordinate.size()) {
         return true;
     }
 }
@@ -118,7 +141,7 @@ void Predictor::get_excercise_time(double x, double y, double v, double ptz_pitc
     time_square =
             2.0 * ((y_bar * g + v * v) - sqrt(pow(g * y_bar + v * v, 2.0) - (x_bar * x_bar + y_bar * y_bar) * g * g)) /
             (g * g);
-    time_for_excercise = sqrt(time_square) + 0.04;
+    time_for_excercise = sqrt(time_square) + 0.01;
     if (isnan(time_for_excercise)) {
         time_for_excercise = 0.04;
     }
