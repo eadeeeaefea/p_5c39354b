d3449300818fd7408244ce2bf33856da042dc08d
MOSSOLBMULP <52730414+MOSSOLBMULP@users.noreply.github.com>
2019-12-30 20:12:55 +0800

    曾婧 步兵代码最终整合
    曾婧：将整合好的anglesolver文件整合在候典泳的最终步兵代码上。

--- include/anglesolver.h
+++ include/anglesolver.h
@@ -14,13 +14,14 @@
 #include <cmath>
 #include <iostream>
 #include "base.h"
+#include "timer.h"
 #ifdef RUNNING_TIME
 #include "timer.h"
 #endif
 
 class AngleSolver {
 private:
-    double yaw_offset_, pitch_offset_;
+    double yaw_offset_, pitch_offset_,g;
     double origin_pitch;
     double origin_yaw;
 
@@ -29,6 +30,7 @@ public:
     ~AngleSolver();
     void init();
     void run(double x, double y, double z, double v, double &yaw, double &pitch, double ptz_pitch);
+
     void setOriginPitch(double pitch);
     void setOriginYaw(double yaw);
     double getOriginPitch();
@@ -39,12 +41,18 @@ public:
     void set_yaw_offset(double yaw_offset);
     void set_pitch_offset(double pitch_offset);
 
-
+    //其他备用算法
+    //转换坐标系加二分法计算函数
+    void change_dichotomy(double x, double y, double z, double v,double& yaw, double& pitch, double ptz_pitch);
+    // 未加坐标旋转变换，稍微慢一些的两个算法，速度矢量三角计算快于二分法。
+    //二分法算法原理同上
+    void dichotomy(double x, double y, double z, double v,double& yaw, double& pitch);
+    void speedtriangle(double x, double y, double z, double v,double& yaw, double& pitch);
 private:
     bool parabolaSolve(double x, double y, double v, double &theta, double ptz_pitch);
-    double parabolaDeltaY(double x, double y, double v, double theta);
 
 };
 
 
 #endif  // HERORM2020_ANGLESOLVER_H
+
